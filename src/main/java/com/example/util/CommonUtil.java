99d7c2f69f2cb9299c99ffc0ba44b566d49f3093
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-09-14 17:31:01 -0500

    feat(Util): Util
    Add base utilities for the app

---
+++ src/main/java/com/example/util/CommonUtil.java
@@ -0,0 +1,43 @@
+package com.example.util;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+
+import static com.example.util.LoggerUtil.log;
+import static java.lang.Thread.sleep;
+
+public class CommonUtil {
+
+    public static StopWatch stopWatch = new StopWatch();
+
+    public static void delay(long delayMilliSeconds)  {
+        try{
+            sleep(delayMilliSeconds);
+        }catch (Exception e){
+            log("Exception is :" + e.getMessage());
+        }
+
+    }
+
+    public static String transForm(String s) {
+        CommonUtil.delay(500);
+        return s.toUpperCase();
+    }
+
+    public static void startTimer(){
+        stopWatch.start();
+    }
+
+    public static void timeTaken(){
+        stopWatch.stop();
+        log("Total Time Taken : " +stopWatch.getTime());
+    }
+
+    public static void stopWatchReset(){
+        stopWatch.reset();
+    }
+
+    public static  int noOfCores(){
+        return Runtime.getRuntime().availableProcessors();
+    }
+}
---
+++ src/main/java/com/example/util/LoggerUtil.java
@@ -0,0 +1,8 @@
+package com.example.util;
+
+public class LoggerUtil {
+
+    public static void log(String message){
+        System.out.println("[" + Thread.currentThread().getName() +"] - " + message);
+    }
+}
