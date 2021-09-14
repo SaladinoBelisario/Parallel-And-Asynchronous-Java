9e51675b17c505ce372bd15f10be89cf91026bfb
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-09-14 18:21:34 -0500

    refactor(Service): ProductService with Threads
    Refactor the ProductService class to use Threads

---
+++ src/main/java/com/example/service/ProductServiceThread.java
@@ -0,0 +1,90 @@
+package com.example.service;
+
+import com.example.domain.Product;
+import com.example.domain.ProductInfo;
+import com.example.domain.Review;
+
+import static com.example.util.CommonUtil.stopWatch;
+import static com.example.util.LoggerUtil.log;
+
+public class ProductServiceThread {
+    private ProductInfoService productInfoService;
+    private ReviewService reviewService;
+
+    public ProductServiceThread(ProductInfoService productInfoService, ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Product retrieveProductDetails(String productId) throws InterruptedException {
+        stopWatch.start();
+
+        ProductInfoRunnable productInfoRunnable = new ProductInfoRunnable(productId);
+        Thread productInfoThread = new Thread(productInfoRunnable);
+
+        ReviewRunnable reviewRunnable = new ReviewRunnable(productId);
+        Thread reviewThread = new Thread(reviewRunnable);
+
+        productInfoThread.start();
+        reviewThread.start();
+
+        productInfoThread.join();
+        reviewThread.join();
+
+
+
+        ProductInfo productInfo = productInfoRunnable.getProductInfo();// blocking call
+        Review review = reviewRunnable.getReview(); // blocking call
+
+        stopWatch.stop();
+        log("Total Time Taken : "+ stopWatch.getTime());
+        return new Product(productId, productInfo, review);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ProductInfoService productInfoService = new ProductInfoService();
+        ReviewService reviewService = new ReviewService();
+        ProductServiceThread productService = new ProductServiceThread(productInfoService, reviewService);
+        String productId = "ABC123";
+        Product product = productService.retrieveProductDetails(productId);
+        log("Product is " + product);
+
+    }
+
+    private class ProductInfoRunnable implements Runnable {
+        private ProductInfo productInfo;
+        private String productId;
+
+        public ProductInfoRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        public ProductInfo getProductInfo() {
+            return productInfo;
+        }
+
+        @Override
+        public void run() {
+            productInfo = productInfoService.retrieveProductInfo(productId);
+        }
+    }
+
+    private class ReviewRunnable implements Runnable {
+        private String productId;
+        private Review review;
+
+        public ReviewRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        public Review getReview() {
+            return review;
+        }
+
+        @Override
+        public void run() {
+            review = reviewService.retrieveReviews(productId);
+        }
+    }
+}
